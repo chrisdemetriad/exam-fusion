77ae16d632a98e06001fa6894cc22c1b387f12b6
Chris Demetriad <chris@demetriad.co.uk>
2024-10-08 00:34:59 +0100

    allowing images from external source in Next config file

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	transpilePackages: ["@tabler/icons-react"],
+	images: {
+		domains: ["avatars.githubusercontent.com"],
+	},
 };
 
 export default nextConfig;
