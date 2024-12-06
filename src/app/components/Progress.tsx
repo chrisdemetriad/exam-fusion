299fa3926e1d51c5d3ee3a4a78811acb0a39d633
Chris Demetriad <chris@demetriad.co.uk>
2024-12-06 10:28:34 +0000

    adding one more chart to the Progress page

--- src/app/components/Progress.tsx
+++ src/app/components/Progress.tsx
@@ -1,10 +1,10 @@
 "use client";
 
-import { Text } from "@mantine/core";
+import { Text, Box } from "@mantine/core";
 import type { Session } from "next-auth";
 import { useSession } from "next-auth/react";
 import { useEffect, useState } from "react";
-import { Bar, BarChart, Cell, LabelList, Legend, ResponsiveContainer, Tooltip, XAxis } from "recharts";
+import { Bar, BarChart, Cell, Legend, ResponsiveContainer, Tooltip, XAxis, LineChart, Line, LabelList } from "recharts";
 import { useTestStore } from "../stores/stateStore";
 import { CustomTooltip } from "./Charts/CustomTooltip";
 
@@ -35,7 +35,7 @@ export const Progress = () => {
 	const [visibleProviders, setVisibleProviders] = useState<string[]>([]);
 	const [providerColors, setProviderColors] = useState<Record<string, string>>({});
 	const baseUrl = useTestStore((state) => state.baseUrl);
-	const colors = ["#8884d8", "#82ca9d", "#ffc658", "#ff8042"];
+	const colors = ["#8884d8", "#82ca9d", "#ffc658", "#ff8042", "#ffcc00", "#00c49f"];
 
 	const { data: session } = useSession() as { data: Session | null };
 	const userEmail = session?.user?.email;
@@ -90,65 +90,83 @@ export const Progress = () => {
 			);
 		}
 	};
-	const labelFormatter = (label: string) => {
-		return label.length > 20 ? `${label.substring(0, 17)}...` : label;
-	};
+
+	const averageScores = Object.keys(providerColors).map((provider) => {
+		const providerData = progressData.filter((item) => item.testId.provider === provider);
+		const averageScore = providerData.reduce((sum, entry) => sum + (entry.score ?? 0), 0) / providerData.length || 0;
+		return { provider, averageScore };
+	});
 
 	return (
-		<div style={{ width: "100%", height: 500 }}>
-			<ResponsiveContainer>
-				<BarChart data={filteredData} margin={{ bottom: 80 }}>
-					<XAxis
-						dataKey={(entry) => `${entry.testId.provider} - ${entry.testId.title}`}
-						angle={-30}
-						textAnchor="end"
-						interval={0}
-						style={{ fontSize: "10px" }}
-						tickFormatter={labelFormatter}
-					/>
-					<Tooltip content={<CustomTooltip />} />
-					{Object.keys(providerColors).map((provider) => (
-						<Bar
-							key={provider}
-							dataKey={(entry) => (entry.testId.provider === provider ? entry.score : null)}
-							fill={providerColors[provider]}
-							stackId="provider"
-						>
-							{filteredData.map((entry, index) => (
-								<Cell
-									key={`cell-${index}`}
-									fill={
-										entry.testId.provider === provider && (entry.score === 0 || entry.score === null)
-											? "red"
-											: providerColors[provider]
-									}
+		<Box>
+			<Text>Daily progress</Text>
+			<Box style={{ width: "100%", height: 400, marginBottom: "2rem" }}>
+				<ResponsiveContainer>
+					<BarChart data={filteredData} margin={{ bottom: 80 }}>
+						<XAxis
+							dataKey={(entry) => `${entry.testId.provider} - ${entry.testId.title}`}
+							angle={-30}
+							textAnchor="end"
+							interval={0}
+							style={{ fontSize: "10px" }}
+						/>
+						<Tooltip content={<CustomTooltip />} />
+						{Object.keys(providerColors).map((provider) => (
+							<Bar
+								key={provider}
+								dataKey={(entry) => (entry.testId.provider === provider ? entry.score : null)}
+								fill={providerColors[provider]}
+								stackId="provider"
+							>
+								{filteredData.map((entry, index) => (
+									<Cell
+										key={`cell-${index}`}
+										fill={
+											entry.testId.provider === provider && (entry.score === 0 || entry.score === null)
+												? "red"
+												: providerColors[provider]
+										}
+									/>
+								))}
+								<LabelList
+									dataKey={(entry) => (entry as ProgressData).testId.provider}
+									position="insideBottom"
+									style={{ fontSize: "10px", fill: "#fff" }}
 								/>
-							))}
-							<LabelList
-								dataKey={(entry) => (entry as ProgressData).testId.provider}
-								position="insideBottom"
-								style={{ fontSize: "10px", fill: "#fff" }}
-							/>
-						</Bar>
-					))}
-					<Legend
-						layout="horizontal"
-						align="center"
-						verticalAlign="top"
-						payload={Object.keys(providerColors).map((provider) => ({
-							id: provider,
-							value: provider,
-							type: "square",
-							color: visibleProviders.includes(provider) ? providerColors[provider] : "#d3d3d3",
-						}))}
-						onClick={(data, index, event) => {
-							if (data?.id) {
-								handleLegendClick(data.id);
-							}
-						}}
-					/>
-				</BarChart>
-			</ResponsiveContainer>
-		</div>
+							</Bar>
+						))}
+						<Legend
+							layout="horizontal"
+							align="center"
+							verticalAlign="top"
+							payload={Object.keys(providerColors).map((provider) => ({
+								id: provider,
+								value: provider,
+								type: "square",
+								color: visibleProviders.includes(provider) ? providerColors[provider] : "#d3d3d3",
+							}))}
+							onClick={(data, index, event) => {
+								if (data?.id) {
+									handleLegendClick(data.id);
+								}
+							}}
+						/>
+					</BarChart>
+				</ResponsiveContainer>
+			</Box>
+
+			<Text>Average scores</Text>
+			<Box style={{ width: "100%", height: 300, marginBottom: "2rem" }}>
+				<ResponsiveContainer>
+					<LineChart data={averageScores} margin={{ left: 10, right: 10 }}>
+						{/* <CartesianGrid strokeDasharray="3 3" /> */}
+						<XAxis dataKey="provider" padding={{ left: 10, right: 10 }} tickMargin={10} />
+						{/* <YAxis /> */}
+						<Tooltip />
+						<Line type="monotone" dataKey="averageScore" stroke="#8884d8" />
+					</LineChart>
+				</ResponsiveContainer>
+			</Box>
+		</Box>
 	);
 };
