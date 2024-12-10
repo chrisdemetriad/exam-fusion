d7760ff4d1e0ee03f26a98099891371367b2daba
Chris Demetriad <chris@demetriad.co.uk>
2024-12-10 18:22:00 +0000

    cleaning up fonts and styles

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,37 +1,33 @@
 import { Box, ColorSchemeScript, Flex, MantineProvider, rem } from "@mantine/core";
 import "@mantine/core/styles.css";
 import type { Metadata } from "next";
-import localFont from "next/font/local";
+import { Comfortaa, Mulish } from "next/font/google";
 import type React from "react";
 import "./globals.css";
 // import '@mantine/dates/styles.css';
 import { getServerSession } from "next-auth";
-import { Comfortaa } from "next/font/google";
 import Header from "./components/Header";
 import { Navbar } from "./components/NavBar";
 import SessionProvider from "./components/SessionProvider";
-
-const geistSans = localFont({
-	src: "./fonts/GeistVF.woff",
-	variable: "--font-geist-sans",
-	weight: "100 900",
-});
-const geistMono = localFont({
-	src: "./fonts/GeistMonoVF.woff",
-	variable: "--font-geist-mono",
-	weight: "100 900",
-});
 
 const comfortaa = Comfortaa({
 	weight: ["400", "700"],
 	subsets: ["latin"],
 	variable: "--font-comfortaa",
+	display: "swap",
+});
+
+const mulish = Mulish({
+	weight: ["300", "400", "600", "700"],
+	subsets: ["latin"],
+	variable: "--font-mulish",
+	display: "swap",
 });
 
 export const metadata: Metadata = {
-	title: "Exam Fusion",
+	title: "ExamFusion",
 	description:
-		"Exam Practice Hub offers a comprehensive collection of practice questions to help students prepare for their exams. Enhance your knowledge, test your skills, and achieve academic success with our expertly curated questions and detailed explanations.",
+		"ExamFusion offers a comprehensive collection of practice questions to help students prepare for their exams. Enhance your knowledge, test your skills and achieve academic success with our expertly curated questions and detailed explanations.",
 };
 
 export default async function RootLayout({
@@ -40,14 +36,15 @@ export default async function RootLayout({
 	children: React.ReactNode;
 }>) {
 	const session = await getServerSession();
+
 	return (
-		<html lang="en">
+		<html lang="en" className={`${comfortaa.variable} ${mulish.variable}`}>
 			<head>
 				<ColorSchemeScript />
-				<title>{metadata.title?.toString() ?? "Exam Fusion"}</title>
-				<meta name="description" content={metadata.description ?? "Default Description"} />
+				<title>{metadata.title?.toString() || "ExamFusion"}</title>
+				<meta name="description" content={metadata.description || "Practice questions for accountants"} />
 			</head>
-			<body className={`${geistSans.variable} ${geistMono.variable} ${comfortaa.variable}`}>
+			<body>
 				<MantineProvider>
 					<SessionProvider session={session}>
 						<Flex direction="column" style={{ height: "100vh" }}>
@@ -55,7 +52,7 @@ export default async function RootLayout({
 								<Header />
 							</Box>
 							<Flex style={{ flex: "1 1 auto", overflow: "hidden" }}>
-								{session ? <Navbar /> : null}
+								{session && <Navbar />}
 								<main style={{ flex: 1, overflow: "auto", padding: rem(16) }}>{children}</main>
 							</Flex>
 						</Flex>
