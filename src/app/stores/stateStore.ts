b85ccd1dbeecf2d28faf31715ba037aef8cc273c
Chris Demetriad <chris@demetriad.co.uk>
2024-12-11 01:00:47 +0000

    persisting store current test data

--- src/app/stores/stateStore.ts
+++ src/app/stores/stateStore.ts
@@ -1,4 +1,5 @@
 import { create } from "zustand";
+import { persist } from "zustand/middleware";
 import { TestData } from "../components/Tests";
 
 interface AnswerSummary {
@@ -24,19 +25,27 @@ interface TestState {
 	setCurrentTest: (test: TestData) => void;
 }
 
-export const useTestStore = create<TestState>((set) => ({
-	answers: [],
-	duration: 0,
-	modalOpen: false,
-	baseUrl: (process.env.NODE_ENV === "production"
-		? process.env.NEXT_PUBLIC_API_URL_PRODUCTION
-		: process.env.NEXT_PUBLIC_API_URL_LOCAL) as string,
-	currentTest: null,
-	addAnswer: (answer) => set((state) => ({ answers: [...state.answers, answer] })),
-	resetTest: () => set({ answers: [] }),
-	setDuration: (time) => set({ duration: time }),
-	resetDuration: () => set({ duration: 0 }),
-	openModal: () => set({ modalOpen: true }),
-	closeModal: () => set({ modalOpen: false }),
-	setCurrentTest: (test) => set({ currentTest: test }),
-}));
+export const useTestStore = create<TestState>()(
+	persist(
+		(set) => ({
+			answers: [],
+			duration: 0,
+			modalOpen: false,
+			baseUrl: (process.env.NODE_ENV === "production"
+				? process.env.NEXT_PUBLIC_API_URL_PRODUCTION
+				: process.env.NEXT_PUBLIC_API_URL_LOCAL) as string,
+			currentTest: null,
+			addAnswer: (answer) => set((state) => ({ answers: [...state.answers, answer] })),
+			resetTest: () => set({ answers: [] }),
+			setDuration: (time) => set({ duration: time }),
+			resetDuration: () => set({ duration: 0 }),
+			openModal: () => set({ modalOpen: true }),
+			closeModal: () => set({ modalOpen: false }),
+			setCurrentTest: (test) => set({ currentTest: test }),
+		}),
+		{
+			name: "test-store",
+			partialize: (state) => ({ answers: state.answers, currentTest: state.currentTest }),
+		}
+	)
+);
